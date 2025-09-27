273b6a95cf65e31f61c9bd957c8de03514335758
Ankit kumar <iamankitm05@gmail.com>
2025-09-27 18:09:21 +0530

    added image in work section card

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ function toggleNav() {
   const navLinks = document.getElementById('nav-links');
   navLinks.classList.toggle('active');
 }
+
+window.onload = () => {
+  document.querySelector('#portfolio .features').innerHTML = myWorks.map((e) => `
+    <a target="blank" href="${e.link}">
+      <div class="work-card">
+      ${e.image != null ? `<img src="${e.image}" class="work-image" alt="">` : ''}
+        <h3>${e.name}</h3>
+        <p>${e.description}</p>
+      </div>
+    </a>
+  `).join('');
+}
--- my_works.js
+++ my_works.js
@@ -3,21 +3,37 @@ const myWorks = [
     link: "https://www.indusappstore.com/apps/productivity/my-notes/io.github.ankitdotdev.my_notes?page=details&id=io.github.ankitdotdev.my_notes",
     name: "My Notes App",
     description: "A feature-rich Flutter notes app with theming, date-based organization, note creation, editing, completion, deletion, and filtering options.",
+    image: 'https://raw.githubusercontent.com/iamankitm05/my_notes/main/demo.png',
+  },
+  {
+    link: "https://github.com/iamankitm05/ecommerce_app_flutter",
+    name: "E-commerce App UI",
+    description: "A stylish and responsive e-commerce app UI built with Flutter, showcasing product listings and shopping screens.",
+    image: 'https://raw.githubusercontent.com/iamankitm05/ecommerce_app_flutter/main/demo.png'
+  },
+  {
+    link: "https://github.com/iamankitm05/whatsapp_clone_flutter",
+    name: "WhatsApp Clone UI",
+    description: "A clean and responsive WhatsApp-inspired user interface built with Flutter.",
+    image: 'https://raw.githubusercontent.com/iamankitm05/whatsapp_clone_flutter/main/demo.png',
   },
   {
     link: "https://play.google.com/store/apps/details?id=com.music_player.music_player&pcampaignid=web_share",
     name: "Music Player App",
     description: "A Flutter-based music player UI with local audio playback, dark/light themes, and customizable color options.",
+    image: '/images/music_player_icon.png',
   },
   {
     link: "https://play.google.com/store/apps/details?id=in.onespect.onespect_flutter_mobile_app&pcampaignid=web_share",
     name: "Onespect - Learning & Service",
     description: "A full-featured Flutter learning app with email/password authentication, books, schools, quizzes, test series, profile management, and more.",
+    image: '/images/onespect.png',
   },
   {
     link: "https://www.indusappstore.com/apps/health-and-fitness/are-you-colorblind/com.ankitm05.are_you_colorblind/?page=details&id=com.ankitm05.are_you_colorblind",
     name: "Are You Colorblind",
     description: "A feature-rich Flutter app that uses the Ishihara test to help users check for color blindness with themed UI and smooth interactions.",
+    image: '/images/are you color blind.png'
   },
   {
     link: "https://github.com/iamankitm05/animations_flutter",
@@ -25,19 +41,9 @@ const myWorks = [
     description: "A simple Flutter animation project featuring bouncing balls and interactive controls to manipulate their movement.",
   },
   {
-    link: "https://github.com/iamankitm05/whatsapp_clone_flutter",
-    name: "WhatsApp Clone UI",
-    description: "A clean and responsive WhatsApp-inspired user interface built with Flutter.",
-  },
-  {
     link: "https://github.com/iamankitm05/queezy_flutter_app",
     name: "Queezy App UI",
     description: "A sleek and modern quiz app user interface designed in Flutter.",
-  },
-  {
-    link: "https://github.com/iamankitm05/ecommerce_app_flutter",
-    name: "E-commerce App UI",
-    description: "A stylish and responsive e-commerce app UI built with Flutter, showcasing product listings and shopping screens.",
   },
   {
     link: "https://github.com/iamankitm05/easy_tech_flutter",
@@ -46,13 +52,3 @@ const myWorks = [
   },
 ];
 
-window.onload = () => {
-  document.querySelector('#portfolio .features').innerHTML = myWorks.map((e) => `
-    <a target="blank" href="${e.link}">
-      <div class="work-card">
-        <h3>${e.name}</h3>
-        <p>${e.description}</p>
-      </div>
-    </a>
-  `).join('');
-}
